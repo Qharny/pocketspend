10b53c3b5860acee72b95f0a04d8314587d0254d
Qharny <kabuteymanasseh5@gmail.com>
2026-01-24 13:22:24 +0000

    release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,6 +7,9 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+dependencies {
     implementation("com.google.android.material:material:1.10.0")
 }
 
